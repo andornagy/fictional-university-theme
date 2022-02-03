96f291c09540a7ea1878fc0ab5c203b7ee8ddef8
Andor <andornagy2012@gmail.com>
2022-02-03 20:15:33 +0200

    Fixed missing permalinks for blog posts on the front end.

--- front-page.php
+++ front-page.php
@@ -62,7 +62,7 @@
               <span class="event-summary__day"><?php the_time('d') ?></span>
             </a>
             <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#"><?php the_title(); ?></a></h5>
+              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
               <p><?php if (has_excerpt()) { echo get_the_excerpt(); } else { echo wp_trim_words( get_the_content(),18 ); } ?><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"class="nu gray">Read more</a></p>
             </div>
           </div>
